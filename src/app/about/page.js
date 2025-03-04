02d8261f83e2a34ebbcfe5656bfb91d52135153e
JasperMunene <jaspermunene2022@gmail.com>
2025-03-04 14:25:52 +0300

    Changes

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import Footer from "@/components/footer/footer"
+import Hero2 from "@/components/hero2/hero2";
+import FunFact from "@/components/fun-fact/funFact"
+export default function AboutPage(props) {
+    return (
+        <div className="main-wrapper">
+            <Hero2 />
+            <FunFact />
+            <Footer/>
+        </div>
+
+            )
+            }
